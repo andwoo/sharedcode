bc3ddc8ea78c1e44f518715b915d459330ecc7b8
Andrew M <andrewlmanley@gmail.com>
2015-11-08 12:10:16 -0500

    -initial commit of the shared code library.

---
+++ sharedcode/TestClassYup.cs
@@ -0,0 +1,15 @@
+﻿namespace sharedcode
+{
+  public class TestClassYup
+  {
+    public static void TESTYO()
+    {
+
+    }
+
+    public static string TESTRETURN()
+    {
+      return "yup";
+    }
+  }
+}
