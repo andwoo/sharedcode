0d54aa0af558634442159b641c045854dd99819b
Andrew M <andrewlmanley@gmail.com>
2015-11-12 21:44:28 -0500

    -added singleton class and unit tests.

---
+++ sharedcode/Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿namespace sharedcode
+{
+  /// <summary>
+  /// Contains a static singleton reference to the base class.
+  /// </summary>
+  /// <typeparam name="TSingleton">Type of the derived base class.</typeparam>
+  public class Singleton<TSingleton> where TSingleton : new()
+  {
+    /// <summary>
+    /// Singleton reference.
+    /// </summary>
+    private static TSingleton _instance;
+
+    /// <summary>
+    /// Singleton reference.
+    /// </summary>
+    public static TSingleton instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new TSingleton();
+        }
+        return _instance;
+      }
+    }
+  }
+}
---
+++ tests/SingletonTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sharedcode;
+
+namespace tests
+{
+  [TestClass]
+  public class SingletonTests
+  {
+    [TestMethod]
+    public void CreateSingleton()
+    {
+      Assert.IsNotNull(TestSingleton.instance);
+      Assert.AreEqual(TestSingleton.instance.ToString(), "test");
+    }
+  }
+
+  public class TestSingleton : Singleton<TestSingleton>
+  {
+    public override string ToString()
+    {
+      return "test";
+    }
+  }
+}
